11e60b45d667dfe96263947fba5cced40b3be4bd
Matias Alves <matias.alves2002@gmail.com>
2024-10-06 14:09:40 -0300

    CambiosDiseño

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -8,20 +8,21 @@ type ColumnProps = {
 }
 
 const Column: FC<ColumnProps> = ({ title, items }) => {
-
   return (
-    <div className="column">
-      <div className="level">
-        <div className="level-item">
-          <figure className="image is-16x16">
-            <img src="assets/menu-hamburguesa.png" alt="Menu icon" />
-          </figure>
-        </div>
-        <div className="level-item">
-          <h2 className="is-size-4">{title}</h2>
+    <div className="columns-container">
+      <div className="column">
+        <div className="level">
+          <div className="level-item">
+            <figure className="image is-16x16">
+              <img src="../../assets/menu-hamburguesa.png" alt="Menu icon" />
+            </figure>
+          </div>
+          <div className="level-item">
+            <h2 className="is-size-4">{title}</h2>
+          </div>
         </div>
         {items.map((item) => (
-          <Card {...item}/>
+          <Card {...item} />
         ))}
       </div>
     </div>
