2e2534ee74ca1304767e4597cbaf9e775eb79fce
mathiassellanes <mathias@neocoast.com>
2024-10-06 12:34:50 -0300

    add_api_integration

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,27 +1,27 @@
 import axios from 'axios';
 import { CardType } from '../constants/types';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const getCards = async () => {
-  const response = await axios.get(`${API_URL}/cards`);
+  const response = await axios.get(`${API_URL}`);
 
   return response.data;
 }
 
-const post = async (data: CardType) => {
+const postCard = async (data: CardType) => {
   const response = await axios.post(`${API_URL}`, data);
 
   return response.data;
 }
 
-const put = async (id: string, data: CardType) => {
+const putCard = async (id: string, data: CardType) => {
   const response = await axios.put(`${API_URL}/${id}`, data);
 
   return response.data;
 }
 
-const remove = async (id: string) => {
+const deleteCard = async (id: string) => {
   const response = await axios.delete(`${API_URL}/${id}`);
 
   return response.data;
@@ -36,8 +36,8 @@ const updateIndexes = async (cards: CardType[]) => {
 
 export {
   getCards,
-  post,
-  put,
-  remove,
+  postCard,
+  putCard,
+  deleteCard,
   updateIndexes,
 }
--- src/components/FormInputs/input.tsx
+++ src/components/FormInputs/input.tsx
@@ -1,9 +1,49 @@
-const Input = ({ label, ...props }) => {
+import { FC } from "react";
+
+interface InputProps {
+  label: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  error?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  name: string;
+}
+
+const Input: FC<InputProps> = ({
+  label,
+  error,
+  value = '',
+  placeholder = '',
+  type = 'text',
+  onChange,
+  className,
+  name,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+  };
+
   return (
     <div className="field">
       <label className="label">{label}</label>
       <div className="control">
-        <input className="input" {...props} />
+        <input
+          className={`input ${className}`}
+          value={value}
+          onChange={handleChange}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+        />
+        <div className="error">
+          {
+            error && (
+              <p className="help is-danger">{error}</p>
+            )
+          }
+        </div>
       </div>
     </div>
   );
